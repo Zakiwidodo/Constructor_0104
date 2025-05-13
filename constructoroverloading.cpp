e30b46906b5ff3db5ab7f5b7b0df2d48b5bea2ee
Zakiwidodo <muhzakiwidodo@gmail.com>
2025-05-13 16:17:22 +0700

    commit15

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -40,4 +40,8 @@ int main(){
     mahasiswa mhs3(01203, "Santi");
 
     //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+    return 0;
 }
